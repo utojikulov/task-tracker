187369f09e3573d07a2d01d348da1b1bdaac82d2
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-05-26 13:02:08 +0500

    updateTask logic completed

--- src/commands/updateTask.ts
+++ src/commands/updateTask.ts
@@ -1,1 +1,34 @@
-export async function updateTask() {}
+import {
+  validateTaskId,
+  validateTaskStatus,
+} from "../validations/validateTask.js";
+import { loadFile, save } from "../utils/fileHandler.js";
+import { Task } from "../types/interface.task.js";
+
+export async function updateTaskStatus(option: string, id: number) {
+  const validTaskOptions: { [index: string]: any } = {
+    "mark-todo": "todo",
+    "mark-inprogress": "inprogress",
+    "mark-done": "done",
+  };
+  const status = validTaskOptions[option];
+  try {
+    validateTaskStatus(status);
+    validateTaskId(id);
+
+    const tasks: Task[] = await loadFile();
+    const taskIndex = tasks.findIndex((task) => task.id === Number(id));
+
+    if (taskIndex === -1) {
+      throw new Error(`Task with ID ${id} not found.`).message;
+    }
+
+    tasks[taskIndex].status = status;
+    tasks[taskIndex].updatedAt = new Date().toISOString();
+
+    await save(tasks);
+    console.log(`Task with ID: ${id} marked as ${status}`);
+  } catch (error) {
+    console.error("Error updating task:", error);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,33 @@
 import { addTask } from "./commands/addTask.js";
 import { deleteTask } from "./commands/deleteTask.js";
 import { readTask } from "./commands/readTask.js";
-import { updateTask } from "./commands/updateTask.js";
-// import { validateTaskOption } from "./validations/validateTask.js";
+import { updateTaskStatus } from "./commands/updateTask.js";
+import { validateTaskOption } from "./validations/validateTask.js";
 
-const [, , command, arg1, arg2] = process.argv;
+const [, , command, arg] = process.argv;
 
 async function main() {
   switch (command) {
     case "add":
-      await addTask(arg1);
+      await addTask(arg);
       break;
     case "remove":
-      await deleteTask(Number(arg1));
+      await deleteTask(Number(arg));
       break;
-    // case "update":
-    //   await updateTask(arg1, arg2);
-    //   break;
     case "list":
-      await readTask(arg1);
+      await readTask(arg);
       break;
-    // case "mark-todo":
-    //   await updateTask(option, arg1);
-    //   break;
-    // case "mark-done":
-    //   await updateTask(option, arg1);
-    //   break;
-    // case "mark-in-progress":
-    //   await updateTask(option, arg1);
-    //   break;
+    case "mark-todo":
+      await updateTaskStatus(command, Number(arg));
+      break;
+    case "mark-done":
+      await updateTaskStatus(command, Number(arg));
+      break;
+    case "mark-inprogress":
+      await updateTaskStatus(command, Number(arg));
+      break;
     default:
-    // validateTaskOption(option);
+      validateTaskOption(command);
   }
 }
 
